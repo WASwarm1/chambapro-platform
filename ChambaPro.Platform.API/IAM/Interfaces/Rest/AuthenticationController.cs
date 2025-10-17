84ad10137024c0a7f1de5c9825eacfc9f89fe8b6
Werner Lang <u202310003@upc.edu.pe>
2025-10-16 20:58:21 -0500

    feat(iam): add Controllers

---
+++ ChambaPro.Platform.API/IAM/Interfaces/Rest/AuthenticationController.cs
@@ -0,0 +1,66 @@
+using System.Net.Mime;
+using ChambaPro.Platform.API.IAM.Domain.Services;
+using ChambaPro.Platform.API.IAM.Infrastructure.Pipeline.Middleware.Authorization;
+using ChambaPro.Platform.API.IAM.Interfaces.Rest.Resources;
+using ChambaPro.Platform.API.IAM.Interfaces.Rest.Transform;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChambaPro.Platform.API.IAM.Interfaces.Rest;
+
+
+[Authorize]
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IUserCommandService _userCommandService;
+
+    public AuthenticationController(IUserCommandService userCommandService)
+    {
+        _userCommandService = userCommandService;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("sign-in")]
+    public async Task<IActionResult> SignIn([FromBody] SignInResource resource)
+    {
+        var signInCommand = SignInCommandFromResourceAssembler
+            .ToCommandFromResource(resource);
+
+        try
+        {
+            var result = await _userCommandService.Handle(signInCommand);
+            var authenticatedUserResource = AuthenticatedUserResourceFromEntityAssembler
+                .ToResourceFromEntity(result.Item1!, result.Item2);
+
+            return Ok(authenticatedUserResource);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { message = e.Message });
+        }
+    }
+
+    [AllowAnonymous]
+    [HttpPost("sign-up")]
+    public async Task<IActionResult> SignUp([FromBody] SignUpResource resource)
+    {
+        var signUpCommand = SignUpCommandFromResourceAssembler
+            .ToCommandFromResource(resource);
+
+        try
+        {
+            var result = await _userCommandService.Handle(signUpCommand);
+                
+            if (result is null)
+                return BadRequest(new { message = "User registration failed" });
+
+            return Ok(new { message = "User registered successfully" });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { message = e.Message });
+        }
+    }
+}
