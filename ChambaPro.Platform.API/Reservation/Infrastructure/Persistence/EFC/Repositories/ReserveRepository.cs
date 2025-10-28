86fb4929c71d7170fd03f901971ffd01ca1742e3
Werner Lang <u202310003@upc.edu.pe>
2025-10-27 19:14:04 -0500

    feat(reserve): add repository

---
+++ ChambaPro.Platform.API/Reservation/Infrastructure/Persistence/EFC/Repositories/ReserveRepository.cs
@@ -0,0 +1,25 @@
+﻿using Chambapro_backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using ChambaPro.Platform.API.Reservation.Domain.Model.Aggregates;
+using ChambaPro.Platform.API.Reservation.Domain.Repositories;
+using ChambaPro.Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChambaPro.Platform.API.Reservation.Infrastructure.Persistence.EFC.Repositories;
+
+public class ReserveRepository(AppDbContext context)
+    : BaseRepository<Reserve>(context), IReserveRepository
+{
+    public async Task<IEnumerable<Reserve>> FindByClientIdAsync(string clientId)
+    {
+        return await Context.Set<Reserve>()
+            .Where(r => r.ClientId == clientId)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Reserve>> FindByTechnicianIdAsync(string technicianId)
+    {
+        return await Context.Set<Reserve>()
+            .Where(r => r.TechnicianId == technicianId)
+            .ToListAsync();
+    }
+}
