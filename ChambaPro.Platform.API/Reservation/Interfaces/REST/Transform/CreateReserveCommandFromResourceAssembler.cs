f0ce4c6949e5499b304cb24fe14bd07786a8f60b
Werner Lang <u202310003@upc.edu.pe>
2025-10-28 16:53:08 -0500

    fix: typo

--- ChambaPro.Platform.API/Reservation/Interfaces/REST/Transform/CreateReserveCommandFromResourceAssembler.cs
+++ ChambaPro.Platform.API/Reservation/Interfaces/REST/Transform/CreateReserveCommandFromResourceAssembler.cs
@@ -7,7 +7,17 @@ public static class CreateReserveCommandFromResourceAssembler
 {
     public static CreateReserveCommand ToCommandFromResource(CreateReserveResource resource)
     {
-        var time = TimeSpan.Parse(resource.Time);
+        TimeSpan time;
+    
+        if (DateTime.TryParse(resource.Time, out DateTime dateTime))
+        {
+            time = dateTime.TimeOfDay;
+        }
+        else if (!TimeSpan.TryParse(resource.Time, out time))
+        {
+            throw new ArgumentException($"Invalid time format: '{resource.Time}'. " +
+                                        "Use time format 'HH:mm' (e.g., '14:30') or valid DateTime string.");
+        }
         
         return new CreateReserveCommand(
             resource.Date,
