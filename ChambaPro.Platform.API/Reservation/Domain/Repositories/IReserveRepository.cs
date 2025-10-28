708c71b818e47d70b12b8bd7f806ba57d5078f71
Werner Lang <u202310003@upc.edu.pe>
2025-10-27 19:13:45 -0500

    feat(reserve): add interface for repository

---
+++ ChambaPro.Platform.API/Reservation/Domain/Repositories/IReserveRepository.cs
@@ -0,0 +1,10 @@
+﻿using Chambapro_backend.Shared.Domain.Repositories;
+using ChambaPro.Platform.API.Reservation.Domain.Model.Aggregates;
+
+namespace ChambaPro.Platform.API.Reservation.Domain.Repositories;
+
+public interface IReserveRepository : IBaseRepository<Reserve>
+{
+    Task<IEnumerable<Reserve>> FindByClientIdAsync(string clientId);
+    Task<IEnumerable<Reserve>> FindByTechnicianIdAsync(string technicianId);
+}
