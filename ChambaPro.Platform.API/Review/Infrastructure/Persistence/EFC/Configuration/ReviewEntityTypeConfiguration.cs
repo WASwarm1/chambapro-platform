719eca0cce01ed3821eb3d0568f702a7341f8226
jorge <jorgefrantas.1@gmail.com>
2025-11-03 14:00:40 -0500

    feat(review):add Infrastructure layer

---
+++ ChambaPro.Platform.API/Review/Infrastructure/Persistence/EFC/Configuration/ReviewEntityTypeConfiguration.cs
@@ -0,0 +1,36 @@
+using ChambaPro.Platform.API.Review.Domain.Models.Aggregates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChambaPro.Platform.API.Review.Infrastructure.Persistence.EFC.Configuration
+{
+    public class ReviewEntityTypeConfiguration : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            builder.ToTable("Reviews");
+
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.Id)
+                   .IsRequired()
+                   .ValueGeneratedOnAdd();
+
+            builder.Property(r => r.Rating)
+                   .IsRequired();
+
+            builder.Property(r => r.Comment)
+                   .IsRequired(false)
+                   .HasMaxLength(500);
+
+            builder.Property(r => r.TechnicianId)
+                   .IsRequired();
+
+            builder.Property(r => r.ClientId)
+                   .IsRequired();
+
+            builder.Property(r => r.CreationDate)
+                   .IsRequired();
+        }
+    }
+}
