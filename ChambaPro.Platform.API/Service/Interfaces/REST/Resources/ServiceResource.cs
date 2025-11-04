06c1335b8613a9fed1c91ab75d75f4dee7706f56
Werner Lang <u202310003@upc.edu.pe>
2025-11-03 21:22:21 -0500

    feat(services): add service resource

---
+++ ChambaPro.Platform.API/Service/Interfaces/REST/Resources/ServiceResource.cs
@@ -0,0 +1,15 @@
+﻿namespace ChambaPro.Platform.API.Service.Interfaces.REST.Resources;
+
+public record ServiceResource(
+    int Id,
+    int ClientId,
+    int TechnicianId,
+    DateTime Date,
+    string Time,
+    string Description,
+    string Category,
+    string Status,
+    decimal Cost,
+    string Duration,
+    string Address
+);
